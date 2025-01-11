3e96d018925427e8bc66adedd0ca4d67711701ba
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2025-01-11 12:37:30 +0530

    Day6

---
+++ react/src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+//JS block
+
+  return (
+    //HTML block
+
+    <div>Header
+
+    <h1>Hello PC!..</h1>
+
+
+</div>
+
+  )
+}
+
+export default Header
